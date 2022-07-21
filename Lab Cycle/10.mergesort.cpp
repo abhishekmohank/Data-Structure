c849304b688d871bf02201c2040a84e219f95d8c
Abhishek <100741799+abhishekmohank@users.noreply.github.com>
2022-07-21 12:04:11 +0530

    Update 10.mergesort.cpp

--- Lab Cycle/10.mergesort.cpp
+++ Lab Cycle/10.mergesort.cpp
@@ -1,175 +1,70 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-void MERGING(float *A,int R,int LBA,float* B,int S,int LBB,float *C,int LBC)
+void merge(int* array,int begin,int mid,int end)
 {
-    int NA=LBA,NB=LBB,PTR=LBC;
-    int UBA=LBA+R-1,UBB=LBB+S-1;
-
-    while(NA<=UBA && NB<=UBB)
-    {
-        if(A[NA]<B[NB])
-        {
-            C[PTR]=A[NA];
-            NA=NA+1;
-            PTR=PTR+1;
-        }
-        else
-        {
-            C[PTR]=B[NB];
-            NB=NB+1;
-            PTR=PTR+1;
-        }
-    }
-
-    if(NA>UBA)
-    {
-        for(int i=0;i<=UBB-NB;i++)
-        {
-            C[PTR+i]=B[NB+i];
-        }
-    }
-    else
-    {
-        for(int i=0;i<=UBA-NA;i++)
-        {
-            C[PTR+i]=A[NA+i];
-        }
-
-    }
+	int i,j,k;
+	int sortedArray[end];
+	i = begin;
+	j = mid+1;
+	k = begin;
+	while (i<=mid and j<=end){
+		if (array[i]<=array[j]){
+			sortedArray[k] = array[i];
+			i++;
+		}
+		else{
+			sortedArray[k] = array[j];
+			j++;
+		}
+		k++;
+	}
+	if(i>mid){
+		while(j<=end){
+			sortedArray[k] = array[j];
+			j++;
+			k++;
+		}
+	}
+	else{
+		while(i<=mid){
+			sortedArray[k] = array[i];
+			i++;
+			k++;
+		}
+	}
+	for (int var = begin; var <= end; ++var)
+     {
+		array[var] = sortedArray[var];
+	}
 }
 
-void MERGE_PASS(float *A,int N,int L,float *B)
+
+void mergeSort(int* array,int begin,int end)
 {
-    int Q=N/(2*L);
-    int S=2*L*Q;
-    int R=N-S;
-    int LB;
-
-     for(int j= 1;j<=Q;j++)
-    {
-        LB=(2*j-2)*L;
-        MERGING(A,L,LB,A,L,LB+L,B,LB);
-    }
-
-    if(R<=L)
-    {
-        for(int k=0;k<R;k++)
-        {
-            B[S+k]=A[S+k];
-        }
-    }
-    else
-    {
-        MERGING(A,L,S,A,R-L,L+S,B,S);
-    }
+	if(begin==end){
+		return;
+	}
+	int mid = (begin+end)/2;
+	mergeSort(array, begin, mid);
+	mergeSort(array, mid+1, end);
+	merge(array,begin,mid,end);
 }
 
-void MERGE_SORT(float *A,float *B,int N)
-{
-    int L=1;
-    
-    while(L<N)
-    {
-        MERGE_PASS(A,N,L,B);
-        MERGE_PASS(B,N,2*L,A);
-        L=4*L;
-    }
 
+void printArray(int* data,int length)
+{
+	for (int var = 0; var < length; ++var) 
+    {
+		cout<<data[var]<<" ";
+	}
+	cout<<endl;
 }
 
-int main()
-{
-    int option,n,choice=1;
-    float A[14]={66,33,40,22,55,88,60,11,80,20,50,44,77,30};
-    float B[14];
-    float *C;
-    float *D;
-
-    cout<<"    MERGE SORT "<<endl;
-    cout<<"   "<<endl;
-    while(choice==1)
-    {
-        cout<<"   Choose from the following options to continue"<<endl;
-        cout<<"      1 : Enter a new array"<<endl;
-        cout<<"      2 : Use a default array"<<endl;
-        cout<<"   >>>";
-        cin>>option;
-        
-
-        if(option==1)
-        {
-           cout<<"   Enter the number of elements in the array"<<endl;
-           cout<<"   >>>";
-           cin>>n; 
-
-          C=new float[n];
-          D=new float[n];
-          cout<<"   >>>";
-          for(int i=0;i<n;i++)
-           {
-               cin>>C[i];
-           }
-
-            cout<<"   Array after sorting : "<<endl;
-            cout<<"   >>>";
-            
-            MERGE_SORT(C,D,n);
-            
-            if(float(n/2)==0)
-            {
-                for(int i=0;i<n-1;i++)
-                {
-                    cout<<C[i]<<", ";
-                }
-                cout<<C[n-1]<<endl;
-            }
-            else
-            {
-                for(int i=0;i<n-1;i++)
-                {
-                    cout<<D[i]<<", ";
-                }
-                cout<<D[n-1]<<endl;
-
-            }
-        }
-        
-        else if(option==2)
-        {
-            n=14;
-            cout<<"   Default array : "<<endl;
-            cout<<"   >>>";
-            for(int i=0;i<n-1;i++)
-            {
-                cout<<A[i]<<", ";
-            }
-            cout<<A[n-1]<<endl;
-            cout<<"   Array after sorting : "<<endl;
-            cout<<"   >>>";
-            MERGE_SORT(A,B,n);
-           if(float(n/2)==0)
-            {
-                for(int i=0;i<n-1;i++)
-                {
-                    cout<<A[i]<<", ";
-                }
-                cout<<A[n-1]<<endl;
-            }
-            else
-            {
-                for(int i=0;i<n-1;i++)
-                {
-                    cout<<B[i]<<", ";
-                }
-                cout<<B[n-1]<<endl;
-
-            }
-        }
-        
-        
-
-    }
-
-    return 0;
+int main() {
+	int array[] = {65,51,7,2,8,71,1};
+	int length = sizeof(array)/sizeof(array[0]);
+	mergeSort(array, 0,length-1);
+	printArray(array,length);
+	return 0;
 }
